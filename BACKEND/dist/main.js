e0ba6f807457d667766447aaf6e4808bbf125a03
Dimaroch <juansincich.design@gmail.com>
2025-06-14 16:38:02 -0300

    Conexión a DB Atlas y funcionalidad de registro de usuarios implementada

--- BACKEND/dist/main.js
+++ BACKEND/dist/main.js
@@ -7,18 +7,67 @@ Object.defineProperty(exports, "__esModule", { value: true });
 require("dotenv/config");
 const express_1 = __importDefault(require("express"));
 const mongoose_1 = __importDefault(require("mongoose"));
+const User_1 = __importDefault(require("./db/models/User"));
+const bcryptjs_1 = __importDefault(require("bcryptjs"));
 const app = (0, express_1.default)();
 const PORT = process.env.PORT || 3001;
 mongoose_1.default
     .connect(process.env.MONGODB_URI)
     .then(() => {
-    console.log("¡Conectado a MongoDB Atlas con éxito!"); // Mensaje de éxito
+    console.log("¡Conectado a MongoDB Atlas con éxito!");
 })
     .catch((error) => {
-    console.error("Error al conectar a MongoDB Atlas:", error.message); // Mensaje de error
-    process.exit(1); // Sale de la aplicación si no se puede conectar a la DB
+    console.error("Error al conectar a MongoDB Atlas:", error.message); //Mensaje de error
+    process.exit(1); //Sale de la aplicación si no se puede conectar a la DB
 });
 app.use(express_1.default.json());
+app.post("/register", async (req, res) => {
+    const { username, email, password } = req.body;
+    try {
+        //1ro verificar si el usuario/email ya existe
+        const existingUser = await User_1.default.findOne({ $or: [{ email }, { username }] });
+        if (existingUser) {
+            if (existingUser.email === email) {
+                return res.status(409).json({ message: "El email ya está registrado" });
+            }
+            if (existingUser.username === username) {
+                return res
+                    .status(409)
+                    .json({ message: "El nombre de usuario ya está registrado" });
+            }
+        }
+        //hashear constraseña
+        const salt = await bcryptjs_1.default.genSalt(10);
+        const passwordHash = await bcryptjs_1.default.hash(password, salt);
+        //guardado de usuario
+        const newUser = new User_1.default({
+            username,
+            email,
+            passwordHash,
+        });
+        await newUser.save();
+        console.log("Intento de registro", {
+            username,
+            email,
+            password,
+            passwordHash,
+        });
+        res.status(201).json({
+            message: "Usuario registrado exitosamente",
+            user: {
+                username: newUser.username,
+                email: newUser.email,
+            },
+        });
+    }
+    catch (error) {
+        console.error("Error de intento de registro");
+        res.status(500).json({
+            message: "Error interno del servidor al registrar usuario.",
+            error: error.message,
+        });
+    }
+});
 // Ruta de prueba
 app.get("/", (req, res) => {
     res.send("<h1>¡El servidor está funcionando correctamente!</h1>");
--- BACKEND/src/main.ts
+++ BACKEND/src/main.ts
@@ -5,6 +5,9 @@ import "dotenv/config";
 import express from "express";
 import type { Request, Response } from "express";
 import mongoose from "mongoose";
+import User from "./db/models/User";
+import bcrypt from "bcryptjs";
+import { log } from "console";
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -12,14 +15,66 @@ const PORT = process.env.PORT || 3001;
 mongoose
   .connect(process.env.MONGODB_URI!)
   .then(() => {
-    console.log("¡Conectado a MongoDB Atlas con éxito!"); // Mensaje de éxito
+    console.log("¡Conectado a MongoDB Atlas con éxito!");
   })
   .catch((error) => {
-    console.error("Error al conectar a MongoDB Atlas:", error.message); // Mensaje de error
-    process.exit(1); // Sale de la aplicación si no se puede conectar a la DB
+    console.error("Error al conectar a MongoDB Atlas:", error.message); //Mensaje de error
+    process.exit(1); //Sale de la aplicación si no se puede conectar a la DB
   });
 
 app.use(express.json());
+
+app.post("/register", async (req: Request, res: Response) => {
+  const { username, email, password } = req.body;
+  try {
+    //1ro verificar si el usuario/email ya existe
+    const existingUser = await User.findOne({ $or: [{ email }, { username }] });
+
+    if (existingUser) {
+      if (existingUser.email === email) {
+        return res.status(409).json({ message: "El email ya está registrado" });
+      }
+      if (existingUser.username === username) {
+        return res
+          .status(409)
+          .json({ message: "El nombre de usuario ya está registrado" });
+      }
+    }
+
+    //hashear constraseña
+    const salt = await bcrypt.genSalt(10);
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    //guardado de usuario
+    const newUser = new User({
+      username,
+      email,
+      passwordHash,
+    });
+    await newUser.save();
+
+    console.log("Intento de registro", {
+      username,
+      email,
+      password,
+      passwordHash,
+    });
+
+    res.status(201).json({
+      message: "Usuario registrado exitosamente",
+      user: {
+        username: newUser.username,
+        email: newUser.email,
+      },
+    });
+  } catch (error: any) {
+    console.error("Error de intento de registro");
+    res.status(500).json({
+      message: "Error interno del servidor al registrar usuario.",
+      error: error.message,
+    });
+  }
+});
 
 // Ruta de prueba
 app.get("/", (req: Request, res: Response) => {
